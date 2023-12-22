2d993a1fc078192faa2ced38954597aea0d8179a
Antoine DESRUET <antoine.desruet@epitech.eu>
2023-12-22 23:04:28 +0100

    <type>: <description>

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -4,9 +4,5 @@
 
 int main()
 {
-    ecspressp::QueueImplem<int> queue;
-    queue.Push(2);
-    std::cout << "content == " << queue.Pop() << std::endl;
-    std::cout << "empty == " << queue.IsEmpty() << std::endl;
     return 0;
 }
