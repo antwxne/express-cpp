dd888ba5e3f984d5ec703186f07b87d0a14c6f30
Antoine DESRUET <antoine.desruet@epitech.eu>
2024-01-03 15:49:11 +0100

    <type>: <description>

--- lib/pico.cmake
+++ lib/pico.cmake
@@ -25,6 +25,17 @@ target_include_directories(asio INTERFACE ${asio_SOURCE_DIR}/asio/include)
 find_package(Threads)
 target_link_libraries(asio INTERFACE Threads::Threads)
 
-set(PICO_SRC ${CMAKE_SOURCE_DIR}/lib/picohttpparser/picohttpparser.c)
-include_directories(${CMAKE_SOURCE_DIR}/lib/picohttpparser)
-include_directories(${CMAKE_SOURCE_DIR}/lib/picojson)
+FetchContent_Declare(picohttpparser
+        GIT_REPOSITORY https://github.com/h2o/picohttpparser.git
+        GIT_TAG master
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+)
+FetchContent_GetProperties(picohttpparser)
+if (NOT picohttpparser_POPULATED)
+    FetchContent_Populate(picohttpparser)
+endif ()
+
+#set(PICO_SRC ${CMAKE_SOURCE_DIR}/lib/picohttpparser/picohttpparser.c)
+include_directories(${picohttpparser_SOURCE_DIR})
+#include_directories(${CMAKE_SOURCE_DIR}/lib/picojson)
