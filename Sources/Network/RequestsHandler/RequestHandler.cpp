28f0ca9402e11f7c6fac5096d0f8b5e14c0b9656
Antoine DESRUET <antoine.desruet@epitech.eu>
2024-02-07 16:47:54 +0100

    fix: fix path traversal

--- Sources/Network/RequestsHandler/RequestHandler.cpp
+++ Sources/Network/RequestsHandler/RequestHandler.cpp
@@ -145,7 +145,7 @@ void RequestHandler::GetStaticFile(const HTTPRequest &request,
 
 void RequestHandler::CheckStaticPath(const std::string &path)
 {
-    if (path.find("..") == std::string::npos) {
+    if (path.find("..") != std::string::npos) {
         throw (std::runtime_error(
             std::format("Error: {}. Directory traversal detected.", path)));
     }
